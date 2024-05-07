1db1af7cab76cdc9d5bc6000262feb07ceb4d848
Nicolas Molina <nicolasmolina0920@hotmail.com>
2024-05-07 13:49:25 -0300

    Creating the result of the total

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,11 +1,24 @@
-const OrderTotals = () => {
+import { useMemo } from 'react';
+import { OrderItem } from '../types';
+import { formatCurrency } from '../helpers';
+
+type OrderTotalsProps = {
+  order: OrderItem[];
+};
+
+const OrderTotals = ({ order }: OrderTotalsProps) => {
+  const subtotalAmount = useMemo(
+    () => order.reduce((total, item) => total + item.quantity * item.price, 0),
+    [order]
+  );
+
   return (
     <>
       <div className="space-y-3">
         <h2 className="font-black text-2xl">Totals and tips</h2>
         <p>
           Subtotal payable:
-          <span className="font-bold"> $0</span>
+          <span className="font-bold"> {formatCurrency(subtotalAmount)}</span>
         </p>
         <p>
           Tips:
