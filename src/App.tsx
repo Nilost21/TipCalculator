941eca3be0dfb42df3d098f235d9c0f739d0df7f
Nicolas Molina <nicolasmolina0920@hotmail.com>
2024-05-06 21:17:55 -0300

    Creating the Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css';
 import Header from './components/Header';
+import Main from './components/Main';
+import { menuItems } from './data//db';
 
 function App() {
+  console.log(menuItems);
+
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 }
---
+++ src/components/Main.tsx
@@ -0,0 +1,8 @@
+function Main() {
+  return (
+    <>
+      <h1>Creating the Main</h1>
+    </>
+  );
+}
+export default Main;
